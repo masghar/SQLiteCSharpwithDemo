21c0f5f99ea7a59ba2772e0ed0e3fb17ea7248c1
masghar <m.muhammad.asghar@gmail.com>
2015-03-09 13:46:07 +0500

    Adding bookmarks,deleting bookmarks
    Adding bookmarks,deleting bookmarks

--- SQLiteDemo/SQLiteDemo/Form1.Designer.cs
+++ SQLiteDemo/SQLiteDemo/Form1.Designer.cs
@@ -28,24 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.dgBookMarks = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgDeltebookmarks = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBookMarks)).BeginInit();
             this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDeltebookmarks)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -59,10 +69,13 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(327, 352);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtsearch);
+            this.tabPage1.Controls.Add(this.dgBookMarks);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -71,14 +84,45 @@
             this.tabPage1.Text = "Bookmarks";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Search";
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(49, 3);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(264, 20);
+            this.txtsearch.TabIndex = 1;
+            // 
+            // dgBookMarks
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dgBookMarks.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgBookMarks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBookMarks.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgBookMarks.Location = new System.Drawing.Point(0, 28);
+            this.dgBookMarks.MultiSelect = false;
+            this.dgBookMarks.Name = "dgBookMarks";
+            this.dgBookMarks.ReadOnly = true;
+            this.dgBookMarks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgBookMarks.Size = new System.Drawing.Size(316, 295);
+            this.dgBookMarks.TabIndex = 0;
+            this.dgBookMarks.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgBookMarks_CellContentDoubleClick);
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.button3);
             this.tabPage2.Controls.Add(this.button2);
-            this.tabPage2.Controls.Add(this.button1);
-            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.btnadd);
+            this.tabPage2.Controls.Add(this.txturl);
             this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.txtname);
             this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
@@ -88,73 +132,14 @@
             this.tabPage2.Text = "Add Bookmark";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // button3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(319, 294);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Delete Bookmark";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(319, 294);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Edit Bookmark";
-            this.tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(316, 323);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(75, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(174, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 63);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "URL";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(75, 63);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(174, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Image = global::SQLiteDemo.Properties.Resources.add_contact_icon;
-            this.button1.Location = new System.Drawing.Point(181, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(68, 47);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "ADD";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(75, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Import Bookmarks";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -165,14 +150,110 @@
             this.button2.Text = "Export All Bookmarks";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnadd
             // 
-            this.button3.Location = new System.Drawing.Point(75, 225);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Import Bookmarks";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnadd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnadd.Location = new System.Drawing.Point(236, 89);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(68, 26);
+            this.btnadd.TabIndex = 2;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(47, 63);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(257, 20);
+            this.txturl.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "URL";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(47, 21);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(257, 20);
+            this.txtname.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btnDelete);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.dgDeltebookmarks);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(319, 326);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Delete Bookmark";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(203, 297);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(103, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // dgDeltebookmarks
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dgDeltebookmarks.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgDeltebookmarks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDeltebookmarks.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgDeltebookmarks.Location = new System.Drawing.Point(1, 27);
+            this.dgDeltebookmarks.MultiSelect = false;
+            this.dgDeltebookmarks.Name = "dgDeltebookmarks";
+            this.dgDeltebookmarks.ReadOnly = true;
+            this.dgDeltebookmarks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgDeltebookmarks.Size = new System.Drawing.Size(316, 268);
+            this.dgDeltebookmarks.TabIndex = 1;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(319, 326);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Edit Bookmark";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -184,11 +265,16 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "My Bookmarks Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBookMarks)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDeltebookmarks)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -200,14 +286,20 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgBookMarks;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.TextBox txturl;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtname;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.DataGridView dgDeltebookmarks;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
--- SQLiteDemo/SQLiteDemo/Form1.cs
+++ SQLiteDemo/SQLiteDemo/Form1.cs
@@ -6,14 +6,124 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using System.Diagnostics;
+
 
 namespace SQLiteDemo
 {
     public partial class Form1 : Form
     {
+        SQLiteDatabase db;
+        SQLiteConnection m_dbConnection;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(!File.Exists("bookmarks.s3db")){
+            SQLiteConnection.CreateFile("bookmarks.s3db");
+            CreateDBStructure();
+            }           
+            LoadData();
+        }
+
+        private void CreateDBStructure() {
+
+           
+            m_dbConnection =
+            new SQLiteConnection("Data Source=bookmarks.s3db;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "create table bookmarks (id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, name varchar(200),url varchar(2000), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL )";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+        private void LoadData() {
+
+            try
+            {
+                db = new SQLiteDatabase();
+                DataTable bookmarks;
+                String query = "select id \"id\",name \"name\", url \"url\",";
+                query += "time \"time\"";
+                query += "from bookmarks;";
+                bookmarks = db.GetDataTable(query);
+                // The results can be directly applied to a DataGridView control
+                dgBookMarks.DataSource = bookmarks;
+                dgDeltebookmarks.DataSource = bookmarks;
+                /*
+                // Or looped through for some other reason
+                foreach (DataRow r in recipe.Rows)
+                {
+                    MessageBox.Show(r["Name"].ToString());
+                    MessageBox.Show(r["Description"].ToString());
+                    MessageBox.Show(r["Prep Time"].ToString());
+                    MessageBox.Show(r["Cooking Time"].ToString());
+                }
+	
+                */
+            }
+            catch (Exception fail)
+            {
+                String error = "The following error has occurred:\n\n";
+                error += fail.Message.ToString() + "\n\n";
+                MessageBox.Show(error);
+                this.Close();
+            }
+
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+
+            db = new SQLiteDatabase();
+            Dictionary<String, String> data = new Dictionary<String, String>();
+            data.Add("name", txtname.Text);
+            data.Add("url", txturl.Text);
+            
+            try
+            {
+                db.Insert("bookmarks", data);
+            }
+            catch (Exception crap)
+            {
+                MessageBox.Show(crap.Message);
+            }
+            LoadData();
+            
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgDeltebookmarks.SelectedRows.Count>0)
+            {
+            int id = Convert.ToInt32(dgDeltebookmarks.SelectedRows[0].Cells[0].Value);
+
+                db = new SQLiteDatabase();
+
+                db.Delete("bookmarks", String.Format("id = {0}", id));        
+
+            LoadData();
+            }
+        }
+
+        private void dgBookMarks_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgBookMarks.SelectedRows.Count > 0)
+            {
+                string url = (dgBookMarks.SelectedRows[0].Cells[2].Value).ToString();
+                Process.Start(url);
+            }
         }
     }
 }
